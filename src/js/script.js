26e537655443d44191ac83999c28c0bb1139bc36
MurKat <mknee123@gmail.com>
2018-08-06 13:59:31 -0400

    Add ScrollReveal package so footer navigation fades in on scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,5 @@
 $(document).ready(function() {
     console.log('Ready!');
+
+    ScrollReveal({ distance: '30px' }).reveal('.reveal-index-nav', {delay: 500, origin: 'bottom', easing: 'ease-in-out' });
 });
